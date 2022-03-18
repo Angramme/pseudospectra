06f0ab88764f9437febb0c98e0c7d5c8ceefdb3d
Angramme <kacpero376@gmail.com>
2022-03-18 03:17:35 +0100

    added redundency prevention in tracing

--- lib/algo/tracing.py
+++ lib/algo/tracing.py
@@ -1,5 +1,5 @@
 import numpy as np
-from lib.math import ssvd_min, svd_min
+from lib.math import svd_min, segment_point_distance as segment_d
 from matplotlib.figure import Figure
 
 
@@ -19,14 +19,27 @@ def contours(
 
     # tau = step*min(eps)
     tau = step if step else 0.5*min(eps) 
+    min_eps_dist = step
+    # min_eps_dist = .5 * min([ abs(a-eps[(i+1)%len(eps)]) for i, a in enumerate(eps) ]) if len(eps) > 1 else step
+    print("eps", min_eps_dist)
 
     for i, E in enumerate(eps):
+        bounds = []
+        def is_bound(z):
+            for B in bounds:
+                # iterate over bound segments
+                for i, a in enumerate(B):
+                    b = B[(i+1)%len(B)]
+                    if segment_d(a, b, z) < min_eps_dist: return True
+            return False
+
         for j, Lam in  enumerate(EVs):
             def up(t):
                 p, *ts = t
                 progress = (max(min(p, 0), 1) + len(EVs)*i + j)/(len(eps)*len(EVs))
                 return update((progress, *ts))
-            if not trace_one(
+            
+            bound = trace_one(
                 plot=P,
                 matrix=matrix, 
                 eps=E, 
@@ -35,8 +48,10 @@ def contours(
                 tau=tau,
                 lam0=Lam,
                 update=up, 
-                ):
-                return None
+                is_bound_f=is_bound
+                )
+            if not isinstance(bound, list): return None
+            if len(bound) > 0: bounds.append(bound)
     
     return figure
 
@@ -48,6 +63,7 @@ def trace_one(
     tau: float,
     lam0: complex,
     update = lambda: True, 
+    is_bound_f = None,
     ):
     """
     :param tol: float : relative accuracy of the first point
@@ -78,7 +94,7 @@ def trace_one(
             uz1n, gz1n, vhz1n = g(z1n)
 
             it += 1
-            if it > 500:
+            if it > 250:
                 return None
         
         return z1n, uz1n, gz1n, vhz1n
@@ -98,7 +114,7 @@ def trace_one(
     if not _find_z1: return None
     z_o, u_min_o, s_o, vh_min_o = _find_z1
     z_og = z_o
-
+    if is_bound_f and is_bound_f(z_og): return []
     if not update((0.15,)): return None
     plot.scatter(z_o.real, z_o.imag, s=4.5**2, marker="x", c="red")
 
@@ -132,4 +148,4 @@ def trace_one(
         if not update((.15 + .85*p,)): return None
 
     plot.plot(np.real(boundary), np.imag(boundary), linestyle='-', color='blue')
-    return plot
+    return boundary
