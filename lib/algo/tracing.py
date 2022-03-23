a87b992f46dbf0c675e11d95cf7bb372dfb9ab63
Angramme <kacpero376@gmail.com>
2022-03-23 13:54:11 +0100

    updated tracing algorithm

--- lib/algo/tracing.py
+++ lib/algo/tracing.py
@@ -1,5 +1,6 @@
+from functools import partial
 import numpy as np
-from lib.math import svd_min, segment_point_distance as segment_d
+from lib.math import svd_min, segment_point_distance as segment_d, SegmentGrid, gershgorin
 from matplotlib.figure import Figure
 
 
@@ -19,19 +20,22 @@ def contours(
 
     # tau = step*min(eps)
     tau = step if step else 0.5*min(eps) 
-    min_eps_dist = step
-    # min_eps_dist = .5 * min([ abs(a-eps[(i+1)%len(eps)]) for i, a in enumerate(eps) ]) if len(eps) > 1 else step
-    print("eps", min_eps_dist)
+
+    lb, rb, bb, tb = gershgorin(matrix, np.max(eps))
 
     for i, E in enumerate(eps):
-        bounds = []
+        # bounds = []
+        # def is_bound(z):
+        #     for B in bounds:
+        #         # iterate over bound segments
+        #         for i, a in enumerate(B):
+        #             b = B[(i+1)%len(B)]
+        #             if segment_d(a, b, z) < step: return True
+        #     return False
+
+        sg = SegmentGrid(lb, rb, bb, tb, (rb-lb)/10, tau*3.)
         def is_bound(z):
-            for B in bounds:
-                # iterate over bound segments
-                for i, a in enumerate(B):
-                    b = B[(i+1)%len(B)]
-                    if segment_d(a, b, z) < min_eps_dist: return True
-            return False
+            return sg.is_segment(z)
 
         for j, Lam in  enumerate(EVs):
             def up(t):
@@ -43,7 +47,7 @@ def contours(
                 plot=P,
                 matrix=matrix, 
                 eps=E, 
-                tol=1e-4,
+                tol=1e-2,
                 # tau=0.01,
                 tau=tau,
                 lam0=Lam,
@@ -51,7 +55,8 @@ def contours(
                 is_bound_f=is_bound
                 )
             if not isinstance(bound, list): return None
-            if len(bound) > 0: bounds.append(bound)
+            # if len(bound) > 0: bounds.append(bound)
+            if len(bound) > 0: sg.insert_segment(bound)
     
     return figure
 
@@ -94,7 +99,7 @@ def trace_one(
             uz1n, gz1n, vhz1n = g(z1n)
 
             it += 1
-            if it > 250:
+            if it > 100:
                 return None
         
         return z1n, uz1n, gz1n, vhz1n
@@ -110,7 +115,7 @@ def trace_one(
         print("couldn't find a suitable direction")
         return None
 
-    _find_z1 = find_z1(100)
+    _find_z1 = find_z1(8)
     if not _find_z1: return None
     z_o, u_min_o, s_o, vh_min_o = _find_z1
     z_og = z_o
