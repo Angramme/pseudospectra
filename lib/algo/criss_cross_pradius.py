e577d9168ba43625fa634d3e792d9f7ad7068a24
Angramme <kacpero376@gmail.com>
2022-05-23 22:41:47 +0200

    grahbuamafesf

---
+++ lib/algo/criss_cross_pradius.py
@@ -0,0 +1,85 @@
+import numpy as np
+from collections import Counter
+from lib.math import ssvd_min
+from lib.utils import flat_map
+
+def main(
+    figure, 
+    matrix: np.matrix, 
+    eps: float,
+    step: float,
+    update = lambda: True, 
+    progresstick =.01
+    ):
+    n, _ = matrix.shape
+
+    eps = np.max(eps)
+    P = figure.add_subplot()
+    P.set_aspect(1)
+
+    def solve_max_mag_bound_intersect(theta, eps, A=matrix):
+        As = A.conj().T
+        mat42 = np.vstack((
+            np.hstack(( 1j*np.exp(1j*theta)*As, -eps * np.eye(n)        )),
+            np.hstack(( eps * np.eye(n),        1j*np.exp(-1j*theta)*A  ))
+        ))
+        # TODO: replace with hamiltonian eigenvalue solver
+        EVs42 = np.linalg.eigvals(mat42)
+        intersections = []
+        for (ev, mul) in Counter(EVs42).most_common():
+            if not np.isclose(np.real(ev), 0, rtol=1e-9): continue
+            p = np.imag(ev)*np.exp(1j*theta)
+            sm = ssvd_min(p*np.eye(n) - A) # find min svd
+            if not np.isclose(sm, eps, rtol=1e-9): continue 
+            intersections.append((p, mul))
+        return intersections
+
+
+    def intersections_to_segments(xs, comp=np.real):
+        xs = sorted(xs, key=lambda p: comp(p[0]))
+        segs = []
+        a = None
+        for (x, m) in xs:
+            if(m % 2 == 0): # degenerate point: both beginning and end of a segment!
+                if(a):
+                    segs.append((a, x))
+                    a = x
+                else:
+                    segs.append((x, x))
+            else:
+                if(a):
+                    segs.append((a, x))
+                    a = None
+                else:
+                    a = x
+        return segs
+    
+    EVs = np.linalg.eigvals(matrix)
+    lam = max(EVs, key=np.abs) # eigenvalue with greatest magnitude 
+    P.scatter(lam.real, lam.imag, c="green")
+
+    z1 = solve_max_hor_bound_intersect(1j*np.imag(lam), eps)[0]
+
+    P.scatter(np.real(z1), np.imag(z1), color="cyan")
+
+    zk = z1
+    for k in range(10000):
+        P.scatter(zk.real, zk.imag, c="red")
+        ps = solve_ver_bound_intersect(np.real(zk), eps)
+        psts = [x[0] for x in ps]
+        P.scatter(np.real(psts), np.imag(psts), color="violet")
+        segs = intersections_to_segments(ps, np.imag)
+        mids = [np.real(a) + 1j*np.imag(a+b)/2 for (a, b) in segs]
+        P.scatter(np.real(mids), np.imag(mids), c="yellow")
+        potential = list(map(
+            lambda y: solve_max_hor_bound_intersect(1j*np.imag(y), eps)[0],
+            mids))
+        if len(potential) == 0: break
+        zk1 = max(potential, key=np.real)
+        if np.isclose(zk1, zk, rtol=1e-9):
+            zk = zk1
+            break
+        zk = zk1
+
+    P.scatter(zk.real, zk.imag, c="blue")
+
